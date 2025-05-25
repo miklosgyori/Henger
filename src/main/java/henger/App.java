a3085f1a1779bdeebc1e8ae5dae0b48a6e2c4d59
Miklos Gyori <61111659+miklosgyori@users.noreply.github.com>
2025-05-25 19:23:09 +0200

    App.java as entry point defined
    Application  tested.

--- src/main/java/henger/App.java
+++ src/main/java/henger/App.java
@@ -1,4 +1,16 @@
 package main.java.henger;
 
+import main.java.henger.controller.HengerController;
+import main.java.henger.view.HengerView;
+
+import javax.swing.*;
+
 public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            HengerView view = new HengerView();
+            new HengerController(view);
+            view.setVisible(true);
+        });
+    }
 }
